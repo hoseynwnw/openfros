1b7469a946d8572d297f63c056e6c886fa3a73b9
derry <destan19@126.com>
2022-04-22 16:59:58 +0800

    update build script

--- build2.sh
+++ build2.sh
@@ -1,16 +1,5 @@
-#product_list="xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2"
-#product_list="redmi_ac2100 xiaomi_ac2100 newifi3 xiaomi_660x hiwifi_5962  hiwifi_hc5661  k2p  xiaomi_3g  xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2 jdyun_128 jdyun"
-product_list="redmi_ac2100 xiaomi_ac2100 newifi3 xiaomi_660x hiwifi_5962 k2p  xiaomi_3g  xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2 gehua"
-#product_list="xiaomi_3g  xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2 "
-#product_list="hiwifi_hc5661  k2p  xiaomi_3g  xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2"
-#product_list="redmi_ac2100 newifi3 xiaomi_ac2100 xiaomi_660x hiwifi_5962 xiaomi_3gpro xiaomi_r4a xiaomi_r3gv2 youku_l2 xiaoyu_c5"
-#product_list="xiaomi_3g xiaomi_4"
-#product_list="x86_64"
-#product_list="xiaomi_ax3600"
-#product_list="jdyun jdyun_128"
-#product_list="bcm2710_rpi"
-#product_list="orange_pi"
-#product_list="k3"
+
+product_list="x86_64 r2s r4s k3 bcm2710_rpi redmi_ac2100 x86_64 jdyun jdyun_128 cm520 redmi_ac2100 xiaomi_ac2100 newifi3 xiaomi_660x hiwifi_5962 k2p  xiaomi_3g  xiaomi_3gpro  xiaomi_4  xiaomi_r3gv2  xiaomi_r4a  xiaoyu_c5  youku_l2 gehua"
 
 rlog(){
     date_str=`date`
@@ -26,7 +15,10 @@ for p in $product_list; do
 	echo "product=$p"
 	rlog "begin build product $p"
 	rm tmp -fr
-	make product=$p  -j$core
+	sed -i '/CONFIG_PACKAGE_kmod-app_delay/d' product/$p/product_config
+	sed -i '/CONFIG_PACKAGE_luci-app-app_delay/d' product/$p/product_config
+	echo "CONFIG_PACKAGE_kmod-app_delay=y" >>product/$p/product_config
+	make product=$p  -j$core V=s
 	if [ $? -ne 0 ];then
 		rlog "build product $p failed."
 		exit
